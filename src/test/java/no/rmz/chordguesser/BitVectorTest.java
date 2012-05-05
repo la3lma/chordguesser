85bf60ce0f30b2abac8392047b2add2bb533bb9d
Bjorn Remseth <la3lma@gmail.com>
2012-05-06 01:31:54 +0200

    Adding some more tests

--- src/test/java/no/rmz/chordguesser/BitVectorTest.java
+++ src/test/java/no/rmz/chordguesser/BitVectorTest.java
@@ -57,15 +57,28 @@ public class BitVectorTest {
         instance.setFromString("01");
     }
 
+    @Test
     public void testReadingAZeroFromString() {
         final BitVector instance = new BitVector(1);
         instance.setFromString("0");
         assertFalse("Expected zero-bit to be false", instance.read(0));
     }
 
+    @Test
     public void testReadingAOneFromString() {
         final BitVector instance = new BitVector(1);
         instance.setFromString("1");
-        assertTrue("Expected zero-bit to be false", instance.read(0));
+        assertTrue("Expected zero-bit to be true", instance.read(0));
+    }
+
+    @Test
+    public void testReading01011BitvectorFromString() {
+        final BitVector instance = new BitVector(5);
+        instance.setFromString("01011");
+        assertFalse("Expected zero-bit to be false", instance.read(0));
+        assertTrue("Expected one-bit to be true", instance.read(1));
+        assertFalse("Expected two-bit to be false", instance.read(2));
+        assertTrue("Expected three-bit to be false", instance.read(3));
+        assertTrue("Expected four-bit to be false", instance.read(4));
     }
 }
