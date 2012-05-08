a4957363f0b93150e20cae44de2ae6fa1e504f26
Bjorn Remseth <la3lma@gmail.com>
2012-05-08 10:30:03 +0200

    Add insertion of a chord, but so far no recognition

--- src/test/java/no/rmz/chordguesser/MidiMessageDecoderTest.java
+++ src/test/java/no/rmz/chordguesser/MidiMessageDecoderTest.java
@@ -77,7 +77,7 @@ public final class MidiMessageDecoderTest {
     }
 
     @Mock NoteListener noteListener;
-    
+
     /**
      * Test of decode method, of class MidiMessageDecoder.
      */
@@ -91,18 +91,68 @@ public final class MidiMessageDecoderTest {
         verify(noteListener).noteOn(60);
     }
     
+    private final static byte MIDI_TONE_DOWN_CNAN_ZERO = (byte)0x90;
+
     /**
      * Test of decode method, of class MidiMessageDecoder.
      */
     @Test
     public void testDecodeAllTonesCToneOn() {
         for (int tone = 0; tone < 128; tone++) {
-            final byte[] m = new byte[]{(byte) 0x90, (byte) tone, (byte) 60};
+            final byte[] m = new byte[]{MIDI_TONE_DOWN_CNAN_ZERO, (byte) tone, (byte) 60};
             final long timestamp = 4711L;
             noteListener = mock(NoteListener.class);
             final MidiMessageDecoder instance = new MidiMessageDecoder(noteListener);
             instance.decode(m, timestamp);
             verify(noteListener).noteOn(tone);
         }
+    }
+
+
+    /**
+     * Inject a midi sequence into a message decoder
+     * @param decoder
+     * @param sequence 
+     */
+    private  static void inject(
+            final MidiMessageDecoder decoder, 
+            final byte [][] sequence) {
+        long  timestamp = 0;
+        for (final byte [] signal : sequence) {
+            decoder.decode(signal, timestamp++);
+        }
+    }
+    
+    /**
+     * From a vector of tones, generate a midi sequence of tones
+     * @param tones vector of tones.
+     * @return  Vector of tone down events for channen zero, for the tones
+     *          in the input vector.
+     */
+    private byte [][] midiToneDownSequenceForTones(byte [] tones) {
+        final byte [][] result = new byte [tones.length][];
+        final byte duration = (byte) 100;
+        for (int i = 0 ; i < tones.length ; i++) {
+            result[i] =  new byte[]{MIDI_TONE_DOWN_CNAN_ZERO, tones[i], duration}; 
+        }
+        return result;
+    }
+    
+    /**
+     * This is an exploratory test, when the exploration is done it
+     * probably doesn't even belong in this class.
+     */
+    @Test
+    public void testCMajorChordDetection(){
+        byte [][] cMajorChordArpeggiated =
+                // XXX Replae 60 withsome constant.
+                midiToneDownSequenceForTones(new byte[]{60, 64, 67});
+        
+        
+        final MidiMessageDecoder instance = new MidiMessageDecoder(noteListener);
+        inject(instance, cMajorChordArpeggiated);
+        verify(noteListener).noteOn(60);
+        verify(noteListener).noteOn(64);
+        verify(noteListener).noteOn(67);
     }
 }
