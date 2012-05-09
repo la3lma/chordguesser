0349a4d84240ce3aab80758991d7d8ce1d1f190d
Bjorn Remseth <la3lma@gmail.com>
2012-05-09 08:42:09 +0200

    Witespace

--- src/test/java/no/rmz/chordguesser/ChordAndScaleDatabaseTest.java
+++ src/test/java/no/rmz/chordguesser/ChordAndScaleDatabaseTest.java
@@ -6,11 +6,11 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ChordAndScaleDatabaseTest {
-   
-    
+
+
     private  List<ScaleBean> entries;
     private  ChordAndScaleDatabase chordDb;
-    
+
     @Before
     public void seUp() throws Exception {
         final ScaleCsvReader scr = new ScaleCsvReader();
@@ -28,13 +28,13 @@ public class ChordAndScaleDatabaseTest {
         // XXX This is no way to fix an off by one error!!!
         assertEquals(entries.size() -1 , chordDb.noOfScales());
     }
-    
+
     @Test
     public void testRecognizeCMajorArpeggiated() {
-        
+
         final BitVector cMajorBitvector = new BitVector("100010010000");
         final Set<ScaleBean> matchingScales = chordDb.getMatchingScales(cMajorBitvector);
-        
+
         assert(!matchingScales.isEmpty());
         assertEquals(1, matchingScales.size());
         final  ScaleBean cMajorChord = matchingScales.iterator().next();
