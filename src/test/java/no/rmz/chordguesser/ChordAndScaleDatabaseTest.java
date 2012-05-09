de97be97d2e9ce117d7d4c42e81b2af0556ffeda
Bjorn Remseth <la3lma@gmail.com>
2012-05-09 08:46:12 +0200

    Dotting i-s, dashing t-s, whitespace and comments.

--- src/test/java/no/rmz/chordguesser/ChordAndScaleDatabaseTest.java
+++ src/test/java/no/rmz/chordguesser/ChordAndScaleDatabaseTest.java
@@ -8,11 +8,24 @@ import static org.junit.Assert.*;
 public class ChordAndScaleDatabaseTest {
 
 
-    private  List<ScaleBean> entries;
+    /**
+     * All of the entries in no particular order.
+     */
+    private  List<ScaleBean>       entries;
+
+    /**
+     * The database holding indexed versions of the entries.
+     */
     private  ChordAndScaleDatabase chordDb;
 
+    /**
+     * Read the database content from a CSV file and stuff it in
+     * a database instance.  Throws an exception if somethings goes
+     * wrong.
+     * @throws Exception
+     */
     @Before
-    public void seUp() throws Exception {
+    public final void seUp() throws Exception {
         final ScaleCsvReader scr = new ScaleCsvReader();
         entries = scr.readScalesFromResourceCsv();
         assertNotNull(entries);
@@ -22,20 +35,27 @@ public class ChordAndScaleDatabaseTest {
 
     /**
      * Test of populate method, of class ChordDatabase.
+     * @throws Exception   when something goes wrong
      */
     @Test
-    public void testPopulate() throws Exception {
+    public final void testPopulate() throws Exception {
         // XXX This is no way to fix an off by one error!!!
-        assertEquals(entries.size() -1 , chordDb.noOfScales());
+        assertEquals(entries.size() - 1 , chordDb.noOfScales());
     }
 
+    /**
+     * Just check if we can recognize a c major chord from its
+     * bitpattern.  That being a nice smoketest for most of the
+     * recognition system.
+     */
     @Test
-    public void testRecognizeCMajorArpeggiated() {
+    public final void testRecognizeCMajorArpeggiated() {
 
         final BitVector cMajorBitvector = new BitVector("100010010000");
-        final Set<ScaleBean> matchingScales = chordDb.getMatchingScales(cMajorBitvector);
+        final Set<ScaleBean> matchingScales =
+                chordDb.getMatchingScales(cMajorBitvector);
 
-        assert(!matchingScales.isEmpty());
+        assertTrue(!matchingScales.isEmpty());
         assertEquals(1, matchingScales.size());
         final  ScaleBean cMajorChord = matchingScales.iterator().next();
         assertEquals("Chord C Major", cMajorChord.getAlternativeScaleNames());
