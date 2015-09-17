89772b0df31fbadfc00f21607eddcaab831c919c
Bjørn Remseth <rmz@comoyo.com>
2015-09-17 23:46:44 +0200

    Cleaning up a bit.

--- src/main/java/no/rmz/sequencer/EventGenerator.java
+++ src/main/java/no/rmz/sequencer/EventGenerator.java
@@ -1,21 +1,20 @@
 package no.rmz.sequencer;
 
+import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiUnavailableException;
 
 
 public  final class EventGenerator {
     
-    private final static String IAC_DEVICE_NAME = "IAC devices";
     private final static String IAC_BUS_NAME = "Bus 1";
     
     public final static void main(final String[] argv) throws MidiUnavailableException, SequencerException{
       
-        IacDeviceUtilities.listReceiverDevices();
-        /*
-        final MidiDevice midiDevice = 
-                IacDeviceUtilities.getMidiDevice(IAC_DEVICE_NAME, IAC_BUS_NAME);
+        
+       
+        final MidiDevice midiDevice = IacDeviceUtilities.getMidiReceivingDevice(IAC_BUS_NAME);
         final HackedUpSequencer seq = new HackedUpSequencer(midiDevice);
         seq.start();
-        */
+       
     }
 }
--- src/main/java/no/rmz/sequencer/IacDeviceUtilities.java
+++ src/main/java/no/rmz/sequencer/IacDeviceUtilities.java
@@ -1,72 +1,43 @@
 package no.rmz.sequencer;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 
 /**
- *  Finds an IAC bus in an IAC de  device, and 
- *  returns an instance with an opened midi device.
+ * Finds an IAC bus in an IAC de device, and returns an instance with an opened
+ * midi device.
  */
 public final class IacDeviceUtilities {
 
     /**
      * Utility class, no creator.
      */
-    private IacDeviceUtilities(){}
-
-    public  static MidiDevice getMidiDevice(
-            final String iacDeviceName,
-            final String iacBusName) throws SequencerException {
-        checkNotNull(iacBusName);
-        checkNotNull(iacDeviceName);
-
-        final MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
-
-        for (MidiDevice.Info info : infos) {
-            String desc = info.getDescription();
-            String vendor = info.getVendor();
-            String version = info.getVersion();
-            final String name = info.getName();
-            if (iacBusName.equals(name)) {
-                final MidiDevice device;
-                System.out.println(info.toString());
-                try {
-                    device = MidiSystem.getMidiDevice(info);
-                    device.open();
-                } catch (MidiUnavailableException ex) {
-                    throw new SequencerException(
-                            "Midi unavailable for device " + name, ex);
-                }
-               
-                if (device.getMaxReceivers() != 0) { 
-                    return device;
-                } else {
-                    throw new SequencerException("Couldn't get receiver for device " + device);
-                }
-            }
-        }
-        throw new SequencerException("Couldn't  find IAC midi device: "
-                + iacDeviceName + ", "
-                + iacBusName);
+    private IacDeviceUtilities() {
     }
-    
-    
-    public static void listReceiverDevices() throws MidiUnavailableException {
-        MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
-        for (MidiDevice.Info info : infos) {
-            MidiDevice device = MidiSystem.getMidiDevice(info);
-            if (device.getMaxReceivers() != 0) {
-                final String deviceName = device.getDeviceInfo().getName();
-                System.out.println(deviceName
-                        + " has receivers");
-                
-                if ("Bus 1".equals(deviceName)) {
-                    device.open();
-                    HackedUpSequencer.startSequencer(device);
+
+    /**
+     * Get a midi device ready to receive input.
+     * @param devicename Name of midi device.
+     * @return A MidiDevice instance, opened, ready to receive input.
+     * @throws SequencerException if midi device is unavailable or has no receivers.
+     */
+    public static MidiDevice getMidiReceivingDevice(final String devicename) throws SequencerException {
+        final MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
+        for (final MidiDevice.Info info : infos) {
+            try {
+                final MidiDevice device = MidiSystem.getMidiDevice(info);
+                if (device.getMaxReceivers() != 0) {
+                    final String deviceName = device.getDeviceInfo().getName();
+                    if (devicename.equals(deviceName)) {
+                        device.open();
+                        return device;
+                    }
                 }
+            } catch (MidiUnavailableException ex) {
+                throw new SequencerException("Midi unavailable for  : " + devicename, ex);
             }
         }
+        throw new SequencerException("Couldn't find midi device : " + devicename);
     }
 }
