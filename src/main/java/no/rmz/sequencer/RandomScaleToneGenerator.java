58d7dcf68057bce27c8a68f0cfc2d2b6b4ce8631
Bjørn Remseth <rmz@comoyo.com>
2017-10-30 21:12:51 +0100

    Random hackery

--- src/main/java/no/rmz/sequencer/RandomScaleToneGenerator.java
+++ src/main/java/no/rmz/sequencer/RandomScaleToneGenerator.java
@@ -1,8 +1,12 @@
 package no.rmz.sequencer;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.TreeSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.Receiver;
 import javax.sound.midi.ShortMessage;
@@ -13,6 +17,50 @@ public final class RandomScaleToneGenerator implements SoundGenerator {
     private final List<ShortMessage> myMsgs;
     private final ScaleBean myScale;
     
+    
+   public final class MessageHolder implements Comparable<MessageHolder> {
+       private final long timestamp;
+       private ShortMessage msg;
+
+        public MessageHolder(final long timestamp, final ShortMessage msg) {
+            if (timestamp < 0 ) {
+                throw new IllegalArgumentException(
+                        "negative timestamp " + timestamp);
+            }
+            this.timestamp = timestamp;
+            this.msg = checkNotNull(msg);
+        }
+
+        @Override
+        public int compareTo(final MessageHolder o) {
+            checkNotNull(o);
+            return Long.compare(timestamp, o.timestamp);
+        }
+   }
+   
+   // XXX Make a new clocked sequence to ensure that things happen in order.
+   public final class ClockedSequencer {
+       private final TreeSet<MessageHolder> messages = new TreeSet<>();
+       
+       private final ExecutorService es;
+
+        public ClockedSequencer() {
+            this.es = Executors.newSingleThreadExecutor();
+            this.es.submit(new Runnable() {
+
+                @Override
+                public void run() {
+                    
+                }
+            });
+        }
+       
+       
+       
+       public void add(final MessageHolder mh) {
+           messages.add(mh);
+       }
+   }
 
     public RandomScaleToneGenerator(final ScaleBean scale) {
         this.myScale = checkNotNull(scale);
@@ -34,9 +82,15 @@ public final class RandomScaleToneGenerator implements SoundGenerator {
 
     @Override
     public void generate(final Receiver recv) {
-        final long timeStamp =   -1; // XXX Perhaps use this?
-        final ShortMessage myMsg = randomScaleTone();
-        recv.send(myMsg, timeStamp);
+        try {
+            final long timeStamp =   -1; // XXX Perhaps use this?
+            final ShortMessage myMsg = randomScaleTone();
+            recv.send(myMsg, timeStamp);
+            final ShortMessage endMessage = new ShortMessage();
+            endMessage.setMessage(ShortMessage.NOTE_OFF, 0, myMsg.getData1(), 0);
+        } catch (InvalidMidiDataException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
     private ShortMessage randomScaleTone() {
