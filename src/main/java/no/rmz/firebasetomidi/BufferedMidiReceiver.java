fc42b0825198ff91f0979c75fe03f3cefb4d20a8
Bjørn Remseth <rmz@comoyo.com>
2017-11-07 23:58:53 +0100

    src/main/java/no/rmz/firebasetomidi/MidiReceiver.java

---
+++ src/main/java/no/rmz/firebasetomidi/BufferedMidiReceiver.java
@@ -0,0 +1,42 @@
+package no.rmz.firebasetomidi;
+
+import com.google.common.base.Preconditions;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import javax.sound.midi.Receiver;
+import javax.sound.midi.ShortMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class BufferedMidiReceiver implements MidiReceiver {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BufferedMidiReceiver.class);
+
+    private final Receiver recv;
+    private final BlockingQueue<ShortMessage> queue;
+
+    public BufferedMidiReceiver(final Receiver recv) {
+        this.recv = Preconditions.checkNotNull(recv);
+        this.queue = new LinkedBlockingQueue<>(100000);
+        new Thread(() -> {
+            while (true) {
+                try {
+                    sendOldest();
+                } catch (InterruptedException e) {
+                    LOG.error("Interrupted sending of MIDI message", e);
+                }
+            }
+        }).start();
+    }
+
+    private void sendOldest() throws InterruptedException {
+        final ShortMessage msg = queue.take();
+        final long timeStamp = -1;
+        recv.send(msg, timeStamp);
+    }
+
+    @Override
+    public void put(final ShortMessage msg) {
+        queue.add(msg);
+    }
+}
